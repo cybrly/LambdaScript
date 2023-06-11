69c2e10b1830249ff17076ec48a2ec99062bd278
Cybrly <19822901+cybrly@users.noreply.github.com>
2023-06-11 05:08:48 -0400

    Add files via upload

--- lambda.py
+++ lambda.py
@@ -41,46 +41,44 @@ def get_available_instances():
     return available_instances
 
 
-def get_instance_info(number):
+def get_available_instance_data():
     response = requests.get(API_URL + "instance-types", headers={"Authorization": f"Bearer {AUTH_TOKEN}"})
     data = response.json()["data"]
-    instance_info = list(data.items())[number-1]  # Use number-1 to get the correct index
-    instance_type = instance_info[0]
-    instance_region = next(iter(instance_info[1]["regions_with_capacity_available"]), None)
-    return {"name": instance_type, "region": instance_region}
+    available_instances = {}
+    unavailable_instances = []
+    for idx, (instance_type, instance_info) in enumerate(data.items(), start=1):
+        formatted_instance_type = ' '.join(part.capitalize() for part in instance_type.replace('gpu_', '').split('_'))
+        if instance_info["regions_with_capacity_available"]:
+            if '8x' in instance_type:
+                formatted_instance_type = f"{Fore.GREEN}{formatted_instance_type}{Style.RESET_ALL}"
+            available_instances[idx] = {
+                "name": instance_type,
+                "region": next(iter(instance_info["regions_with_capacity_available"]))['name'] if instance_info["regions_with_capacity_available"] else None
+            }
+        else:
+            unavailable_instances.append(f"{idx}. {formatted_instance_type}")
+    return available_instances
+
 
 def start_instance(number):
-    gpu_info = get_instance_info(number)
-    if not gpu_info["region"]:
-        colored_print(f"\nThe selected instance does not have an available region.", Fore.RED)
+    instance_info = get_available_instance_data().get(number)
+    if instance_info is None:
+        colored_print(f"\nThe number you provided does not correspond to an available GPU. Try again.", Fore.RED)
         sys.exit(1)
+
     data = {
-        "region_name": str(gpu_info["region"]["name"]),
-        "instance_type_name": gpu_info["name"],
+        # Just use the region value directly, since it's now a string
+        "region_name": instance_info["region"],
+        "instance_type_name": instance_info["name"],
         "ssh_key_names": os.getenv('SSH_KEY_NAMES').split(',')
     }
+
     response = requests.post(API_URL + "instance-operations/launch", headers={"Authorization": f"Bearer {AUTH_TOKEN}"}, json=data)
     if response.status_code == 200:
         colored_print(f"\nGPU instance started successfully.", Fore.GREEN)
     else:
         error_response = response.json()
         colored_print(f"\nFailed to start GPU instance. Error Code: {error_response['error']['code']}, Error Message: {error_response['error']['message']}", Fore.RED)
-
-
-def stop_instance(instance_id):
-    url = API_URL + "instance-operations/terminate"
-    payload = {"instance_ids": [instance_id]}
-    headers = {
-        "Authorization": f"Bearer {AUTH_TOKEN}",
-        "Content-Type": "application/json"
-    }
-
-    response = requests.post(url, headers=headers, data=json.dumps(payload))
-    if response.status_code == 200:
-        colored_print(f"\nInstance terminated successfully.",Fore.GREEN)
-    else:
-        error_code = response.json()["error"]["code"]
-        colored_print(f"\nFailed to terminate instance. Error Message: {error_code}",Fore.RED)
 
 
 def check_running_instances():
@@ -98,6 +96,20 @@ def check_running_instances():
             print(f"{Fore.BLUE}IP Address:{Style.RESET_ALL} {instance['ip']}")
             print(f"{Fore.BLUE}Status:{Fore.GREEN} {instance['status']}{Style.RESET_ALL}\n")
 
+def stop_instance(instance_id):
+    url = API_URL + "instance-operations/terminate"
+    payload = {"instance_ids": [instance_id]}
+    headers = {
+        "Authorization": f"Bearer {AUTH_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    if response.status_code == 200:
+        colored_print(f"\nInstance terminated successfully.",Fore.GREEN)
+    else:
+        error_code = response.json()["error"]["code"]
+        colored_print(f"\nFailed to terminate instance. Error Message: {error_code}",Fore.RED)
 
 def print_help_menu():
     help_text = """
